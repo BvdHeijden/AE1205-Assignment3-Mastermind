06d98061b50f6f55747955775945ad651b817e87
Bart <vdheijden.bart@gmail.com>
2018-05-24 11:41:52 +0200

    finished

--- Assignment 3.py
+++ Assignment 3.py
@@ -5,3 +5,38 @@ Created on Thu May 24 09:36:56 2018
 @author: vdhei
 """
 
+from mastertools import gencode, goodplace, goodcolour
+
+codelength=4
+maxtries=10
+
+print("\nLet's play Mastermind!")
+while True:
+    secret=gencode()
+    secretstring=''.join(secret)
+    
+    print('\nI have a code of ',codelength,' numbers between 1 and 6.\nYou have ',maxtries,' turn to guess my code.')
+    
+    for turn in range(maxtries):
+        instring=('Guess '+str(turn+1)+' : ')
+        guess=input(instring)
+        guess=list(guess)
+        
+        gp=goodplace(secret,guess)
+        gc=goodcolour(secret,guess)
+        
+        if gp==codelength:
+            print('\t!!!Correct!!!, the code was ',secretstring,'\nYou guessed the code in ',str(turn+1),' tries')
+            break
+        else:
+            print('\tScore: ',gp,' good and ',gc,' good colour in the wrong position')
+        
+        if turn==(maxtries-1):
+            print('\toh no! You failed to guess my code. it was ',secretstring,'.')
+            break
+    
+    again=input("Do you want to play again? (y/n): ")
+    if again!='y':
+        break
+
+    
--- mastertools.py
+++ mastertools.py
@@ -5,15 +5,31 @@ Created on Thu May 24 09:41:29 2018
 @author: vdhei
 """
 from random import randint
+
 def gencode():
     code=[]
     while len(code)<4:
         c=randint(1,6)
-        if c not in code:
-            code.append(c)
+        if str(c) not in code:
+            code.append(str(c))
         
     return code
 
 def goodplace(code,guess):
-    
+    n=0
+    i=0
+    for x in code:
+        if code[i]==guess[i]: n+=1
+        i+=1
         
+    return n
+
+def goodcolour(code,guess):
+    n=0
+    i=0
+    for x in code:
+        if guess[i] in code: n+=1
+        i+=1
+    
+    c=goodplace(code,guess)
+    return n-c
